aa7bd4e87dd8501d71badf8ada4465fbdedb8a92
NeiderPuentes <ricsonearest@gmail.com>
2020-10-11 20:15:34 -0500

    Update PROYECT

--- Ordenacion_De_Arboles/src/ordenacion_de_arboles/Ordenacion_De_Arboles.java
+++ Ordenacion_De_Arboles/src/ordenacion_de_arboles/Ordenacion_De_Arboles.java
@@ -13,43 +13,23 @@ public class Ordenacion_De_Arboles {
 
     /**
      * @param args the command line arguments
-     */
-    public static void main(String[] arg) {
-        ArbolBinario abo = new ArbolBinario();
-        int var;
-        int valor;
-        Scanner texto = new Scanner(System.in);
-        System.out.println(" Insertando los siguientes valores Randomicamente : ");
-        for (int i = 1; i <= 15; i++) {
-            valor = (int) (Math.pow(2, 3) * Math.random() * 10); // Que significa esta linea de codigo
-            System.out.print(valor + " ");
-            abo.agregar(valor);
-        }
+     */ public static void main(String[] ar) {
+        ArbolBinarioOrdenado abo = new ArbolBinarioOrdenado();
 
-        do {
-            System.out.println(" ");
-            System.out.println("1.- Cantidad de nodos del arbol");
-            System.out.println("2.- Cantidad de nodos hoja");
-            System.out.println("3.- Altura del arbol");
-            System.out.println("4.- BALANCE");
-            System.out.println("0.- Salir");
-            var = texto.nextInt();
-            switch (var) {
-                case 1:
-                    System.out.println("Cantidad de nodos del árbol:" + abo.cantidad());
-                    break;
-                case 2:
-                    System.out.println("Cantidad de nodos hoja:" + abo.cantidadNodosHoja());
-                    break;
-                case 3:
-                    System.out.println("La altura del arbol es:" + abo.retornarAltura());
-                    break;
-                case 4:
-                    abo.imprimirBalance();
-                    break;
-            }
-        } while (var != 0);
-        System.out.println("APLICACIÓN FINALIZADA");
+        abo.insertar(100);
+        abo.insertar(50);
+        abo.insertar(25);
+        abo.insertar(115);
+        abo.insertar(75);
+        abo.insertar(150);
+        abo.insertar(110);
+
+        System.out.println("Impresion preorden: ");
+        abo.imprimirPre();
+        System.out.println("Impresion entreorden: ");
+        abo.imprimirEntre();
+        System.out.println("Impresion postorden: ");
+        abo.imprimirPost();
     }
     
 }
